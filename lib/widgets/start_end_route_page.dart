3f8672f68fce7c5b2e4a29080268007c21b65e17
Lucas Suarez <lrsuarez@fassil.com.bo>
2021-04-05 16:09:46 -0400

    added new page for star and end route circuit

--- lib/widgets/start_end_route_page.dart
+++ lib/widgets/start_end_route_page.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 
@@ -6,9 +7,57 @@ class StartEndRoutePage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('StarEndRoutePage'),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(left: 12.0, right: 12.0),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // boton Cancelar
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      child: TextButton(
+                        child: Text('Cancelar'),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Inicio de la ruta',
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        color: Theme.of(context).primaryTextTheme.bodyText1.color
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      '¿Dónde y cúando empiezas a conducir?',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Theme.of(context).primaryTextTheme.bodyText2.color
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ),
         ),
       ),
     );
